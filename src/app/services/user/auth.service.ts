f89fb30b28302e46ab9001a76d2540986b83ef4b
Noel Kenfack <noel.kenfack@yahoo.fr>
2024-07-30 18:26:49 +0200

    update

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -4,7 +4,10 @@ import { Observable } from 'rxjs';
 import { appSettings } from '../../helpers/appSettings';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Credentials' : 'true',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With', })
 };
 
 @Injectable({
